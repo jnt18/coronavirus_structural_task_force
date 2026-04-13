58f073622762ec807f56a8e215dcbf0def88a8b8
jnt18 <jan.m.thiel@gmail.com>
2026-04-13 23:14:31 +0200

    deleted build folder

--- build/lib/cstf/validate/test.py
+++
@@ -1,9 +0,0 @@
-import pandas as pd
-import numpy as np
-from pandas import DataFrame
-from numpy import ndarray
-from rcsbapi.data import DataQuery
-
-
-def func(x: pd.DataFrame, y: DataQuery) -> ndarray:
-    pass
